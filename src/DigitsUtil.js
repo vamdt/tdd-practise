b186149d92ac12c5d52b85c913609b1a89c35152
vamdt <liang6688267@gmail.com>
2013-07-11 07:32:40 +0800

    complete first question

--- src/DigitsUtil.js
+++ src/DigitsUtil.js
@@ -5,4 +5,17 @@ DigitsUtil.compare = function(first_number, second_number) {
 		return "4A0B";
 	}
 
+	var sameDigitsAndSamePositionCount = 0,
+		sameDigitsButWrongPositionCount = 0,
+		first_number_text = first_number.toString(),
+		second_number_text = second_number.toString();
+
+	for (var i = 0; i < first_number_text.length; i++) {
+		if (first_number_text.charAt(i) == second_number_text.charAt(i)) {
+			sameDigitsAndSamePositionCount ++;
+		} else if (first_number_text.indexOf(second_number_text.charAt(i)) != -1) {
+			sameDigitsButWrongPositionCount++;	
+		}
+	};
+	return [sameDigitsAndSamePositionCount, "A", sameDigitsButWrongPositionCount, "B"].join("");
 }
