4500de932ea988007002d9970ce02099acbd65f4
vamdt <liang6688267@gmail.com>
2013-07-11 06:56:38 +0800

    first test case

---
+++ spec/DigitsUtilSpec.js
@@ -0,0 +1,7 @@
+describe("DigitsUtil", function() {
+	it("should return 4A0B when compare 1234 and 1234", function(){
+		expect(DigitsUtil.compare(1234, 1234)).toEqual("4A0B");
+	});
+
+	
+});
---
+++ src/DigitsUtil.js
@@ -0,0 +1,8 @@
+var DigitsUtil = function() {}
+
+DigitsUtil.compare = function(first_number, second_number) {
+	if (first_number == second_number) {
+		return "4A0B";
+	}
+
+}
