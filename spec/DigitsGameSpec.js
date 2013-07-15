024465cbbf400fe2ef1928ba8d03c909116c574e
vamdt <liang6688267@gmail.com>
2013-07-16 07:29:15 +0800

    add another cases for game

--- spec/DigitsGameSpec.js
+++ spec/DigitsGameSpec.js
@@ -6,12 +6,27 @@ describe("DigitsGame", function() {
 		game = new DigitsGame;
 	});	
 
-	it("should return 'win' when input a right number in six times", function(){
+	it("should return 'win' when input a right number <= six times", function(){
 		expect(game.run(1234)).toEqual("win");
-		//expect(game.run(1345, 1456, 1234)).toEqual("win");
+		expect(game.run(1345, 1456, 1234)).toEqual("win");	
 		expect(game.run(1345, 1456, 1278, 1289, 1234)).toEqual("win");
-		//expect(game.run(1345, 1456, 1234, 1289, 2345, 1234)).toEqual("win");
-		expect(game.run(1345, 1456, 1234, 5678, 7890, 1230, 1234)).toEqual("lose");
+		expect(game.run(1345, 1456, 1234, 1289, 2345, 1234)).toEqual("win");
 	});	
+
+	it("should return 'try again' when input a wrong number < six times",function(){
+		expect(game.run(9876)).toEqual('try again');
+		expect(game.run(9876, 7659)).toEqual('try again');
+		expect(game.run(9876, 1456, 1234, 1289, 2345)).toEqual('try again');
+	})
+
+
+	it("should return 'lose' when input a wrong number >= six times",function(){
+		expect(game.run(9876, 1456, 1234, 1289, 2345, 9567)).toEqual('lose');
+		expect(game.run(9876, 7659,1234, 1289, 2345, 9567, 8475)).toEqual('lose');
+	})
+
+	it("should return 'lose' even innput a right number > six times", function(){
+		expect(game.run(9876, 7659,1234, 1289, 2345, 9567, 1234)).toEqual('lose');
+	})
 
 });
